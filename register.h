513d265375b01c24c02bf79db6ade10d4f5b9ce9
Robert J. Rouquette <robert.rouquette@focusvq.com>
2024-03-28 10:25:25 -0500

    migrating to C++

--- register.h
+++ register.h
@@ -5,10 +5,20 @@
 #ifndef GPSDO_REGISTER_H
 #define GPSDO_REGISTER_H
 
+#ifdef __cplusplus
+extern "C" {
+#else
+#define static_assert _Static_assert
+#endif
+
 #include <stdint.h>
 
 #define REGMAP_32(type, fields) union type { struct fields; uint32_t raw; }
 
-#define PAGE_MAP(type, fields) struct type fields; _Static_assert(sizeof(struct type) == 4096, "PERIPHERAL_MAP must be 4096 bytes");
+#define PAGE_MAP(type, fields) struct type fields; static_assert(sizeof(struct type) == 4096, "PERIPHERAL_MAP must be 4096 bytes");
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //GPSDO_REGISTER_H
